434d44c57447ae33551f0a6d458e8ff44f471582
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-06-24 17:19:12 +0300

    init: start project

---
+++ .storybook/preview.ts
@@ -0,0 +1,19 @@
+import '../src/styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+}
+
+export default preview
